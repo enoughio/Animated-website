9ff19421ad807164fd3199ecd3f68062c0b214ef
Ani <turboji007@gmail.com>
2025-09-02 17:04:31 +0530

    added basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home  from './pages/Home'
+import Agence  from './pages/Agence'
+import Projects from './pages/Projects'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-white'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/agence' element={<Agence/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
